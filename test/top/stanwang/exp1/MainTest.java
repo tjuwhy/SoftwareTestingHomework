7944414dc0f29a40a33f6669e35f4567da45041e
tjuwhy <2481101267@qq.com>
2020-03-02 16:40:28 +0800

    first commit

---
+++ test/top/stanwang/exp1/MainTest.java
@@ -0,0 +1,16 @@
+package top.stanwang.exp1;
+
+import org.junit.Assert;
+import org.junit.Test;
+import top.stanwang.exp1.Main;
+
+public class MainTest {
+    @Test
+    public void test(){
+        Assert.assertTrue(Main.takeOut(0));
+        Assert.assertTrue(Main.takeOut(2));
+        Assert.assertTrue(Main.takeOut(20));
+        Assert.assertTrue(Main.takeOut(50));
+        Assert.assertFalse(Main.takeOut(100));
+    }
+}
